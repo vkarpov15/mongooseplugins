993138c56daa494fdd2222d1a28ea22a74cc1ccf
James Lee <uptownhr@gmail.com>
2016-04-01 11:52:30 -0700

    Using docker to containerize app and db.

--- index.js
+++ index.js
@@ -2,20 +2,13 @@
 
 const co = require('co');
 const mkdirp = require('mkdirp');
-const mongodbVersionManager = require('mongodb-version-manager');
+
 const server = require('./lib/server');
-const topologyManager = require('mongodb-topology-manager');
+
 
 co(function*() {
-  const uri = 'mongodb://localhost:27017/plugins';
+  const uri = process.env.MONGODB || 'mongodb://localhost:27017/plugins';
   const port = process.env.PORT || 3000;
-  const mongodPath = `${yield mongodbVersionManager.path}/mongod`;
-  yield callback => mkdirp('./data', callback);
-  const mongod = new topologyManager.Server(mongodPath, {
-    dbpath: './data'
-  });
-
-  yield mongod.start();
 
   yield server(port, uri, console.log);
 
